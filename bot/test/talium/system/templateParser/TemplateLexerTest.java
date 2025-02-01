f02c26d078be05124c59e7030a46c0e739bbd438
orciument <orciument@gmail.com>
2025-02-01 12:16:27 +0100

    impl tests in TemplateLexerTest, add token string fuzzing

--- bot/test/talium/system/templateParser/TemplateLexerTest.java
+++ bot/test/talium/system/templateParser/TemplateLexerTest.java
@@ -1,29 +1,39 @@
 package talium.system.templateParser;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import talium.system.templateParser.exeptions.TemplateSyntaxException;
+import talium.system.templateParser.tokens.TemplateTokenKind;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 // the correct behaviour is not entirely specified, thats why this test is disabled by default.
 // more or less: if a token is started it has to be finished, otherwise it is a syntax error
 // completion of a statement is not yet checked at this stage, so a missing else directive is not an error at this stage
-@Disabled
 public class TemplateLexerTest {
     @Test
     void missing_closing_bracket() {
         TemplateLexer lex = new TemplateLexer("Hello, ${var.name!");
-        System.out.println(lex.parse());
+        try {
+            lex.parse();
+            fail("Should have thrown an TemplateSyntaxException");
+        } catch (TemplateSyntaxException _) {}
     }
 
     @Test
     void missing_dollar() {
-        TemplateLexer lex = new TemplateLexer("Hello, {var.name}!");
-        System.out.println(lex.parse());
+        var tokens = new TemplateLexer("Hello, {var.name}!").parse();
+        assert tokens.size() == 1;
+        assert tokens.getFirst().kind() == TemplateTokenKind.TEXT;
+        assert tokens.getFirst().value().equals("Hello, {var.name}!");
     }
 
     @Test
     void missing_closing_bracket_directive() {
         TemplateLexer lex = new TemplateLexer("Hello, %{ if var.name != \"\" }${var.name}%{ else }unnamed%{ endif ");
-        System.out.println(lex.parse());
+        try {
+            lex.parse();
+            fail("Should have thrown an TemplateSyntaxException");
+        } catch (TemplateSyntaxException _) {}
     }
 
     @Test
@@ -35,6 +45,33 @@ public class TemplateLexerTest {
     @Test
     void malformed_endIf() {
         TemplateLexer lex = new TemplateLexer("Hello, %{ if var.name != \"\" }${var.name}%{ else }unnamed%{ endif dadasdadad }");
-        System.out.println(lex.parse());
+        try {
+            lex.parse();
+            fail("Should have thrown an TemplateSyntaxException");
+        } catch (TemplateSyntaxException _) {}
+    }
+
+    @Test
+    void directive_start_in_text() {
+        try {
+            new TemplateLexer("template text % mehr text").parse();
+            fail("Should have thrown an TemplateSyntaxException");
+        } catch (TemplateSyntaxException _) {}
+        try {
+            new TemplateLexer("template text %c mehr text").parse();
+            fail("Should have thrown an TemplateSyntaxException");
+        } catch (TemplateSyntaxException _) {}
+    }
+
+    @Test
+    void variable_start_in_text() {
+        try {
+            new TemplateLexer("template text $ mehr text").parse();
+            fail("Should have thrown an TemplateSyntaxException");
+        } catch (TemplateSyntaxException _) {}
+        try {
+            new TemplateLexer("template text $c mehr text").parse();
+            fail("Should have thrown an TemplateSyntaxException");
+        } catch (TemplateSyntaxException _) {}
     }
 }
--- bot/test/talium/system/templateParser/TemplateParserTest.java
+++ bot/test/talium/system/templateParser/TemplateParserTest.java
@@ -15,6 +15,50 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class TemplateParserTest {
+    private static final List<String> tokensStrings = new ArrayList<>();
+
+    static {
+        tokensStrings.add(" ");
+        tokensStrings.add(" ");
+        tokensStrings.add(" ");
+        tokensStrings.add(" ");
+        tokensStrings.add(" ");
+        tokensStrings.add(" ");
+        tokensStrings.add("text");
+        tokensStrings.add("string");
+        tokensStrings.add("something else");
+        tokensStrings.add("SPECIAL TEXT");
+        tokensStrings.add("121");
+        tokensStrings.add("1");
+        tokensStrings.add("0");
+        tokensStrings.add("894903890238948389");
+        tokensStrings.add("829.1");
+        tokensStrings.add("2838.19489494");
+        tokensStrings.add("333.33333");
+        tokensStrings.add("true");
+        tokensStrings.add("false");
+        tokensStrings.add("$");
+        tokensStrings.add("${");
+        tokensStrings.add("}");
+        tokensStrings.add("%");
+        tokensStrings.add("%{");
+        tokensStrings.add("}");
+        tokensStrings.add("}");
+        tokensStrings.add("if");
+        tokensStrings.add("else");
+        tokensStrings.add("endif");
+        tokensStrings.add("=");
+        tokensStrings.add("==");
+        tokensStrings.add("!=");
+        tokensStrings.add("<");
+        tokensStrings.add("<=");
+        tokensStrings.add(">");
+        tokensStrings.add(">=");
+        tokensStrings.add("for");
+        tokensStrings.add("endfor");
+        tokensStrings.add(".");
+        tokensStrings.add("\"");
+    }
 
     @Test
     public void fuzzing1M() {
@@ -29,6 +73,37 @@ public class TemplateParserTest {
     @Test
     public void fuzzing10KLong() {
         randomFuzzing(10000, 2000);
+    }
+
+    @Test
+    public void tokenFuzzing1M() {
+        tokenFuzzing(1000000, 20);
+    }
+
+    @Test
+    public void tokenFuzzing10K() {
+        tokenFuzzing(10000, 20);
+    }
+
+    @Test
+    public void tokenFuzzing10KLong() {
+        tokenFuzzing(10000, 200);
+    }
+
+    private void randomFuzzing(int n, int length) {
+        for (int i = 0; i < n; i++) {
+            test(RandomStringUtils.randomAscii(length), i);
+        }
+    }
+
+    private void tokenFuzzing(int n, int length) {
+        for (int i = 0; i < n; i++) {
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < length; j++) {
+                builder.append(tokensStrings.get(RandomUtils.nextInt(tokensStrings.size())));
+            }
+            test(builder.toString(), i);
+        }
     }
 
     private void test(String src, int iteration) {
