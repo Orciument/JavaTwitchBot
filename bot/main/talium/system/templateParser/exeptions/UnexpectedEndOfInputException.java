20d71aa0bbb253b24276fe848890221809ce39ac
orciument <orciument@gmail.com>
2025-02-04 01:58:42 +0100

    remove outdated todos

--- bot/main/talium/system/templateParser/exeptions/UnexpectedEndOfInputException.java
+++ bot/main/talium/system/templateParser/exeptions/UnexpectedEndOfInputException.java
@@ -2,7 +2,6 @@ package talium.system.templateParser.exeptions;
 
 
 public class UnexpectedEndOfInputException extends ParsingException {
-    //TODO maybe this should only be thrown be low level api's and higher level API's should about not getting an expected token
     public UnexpectedEndOfInputException() {
         super();
     }
