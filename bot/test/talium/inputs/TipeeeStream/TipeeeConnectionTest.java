8418d13492e2e26e19725d63ea43d3fd69988258
orciument <orciument@gmail.com>
2025-02-06 17:21:09 +0100

    add TipeeeConnectionTest to test authentication procedure with live tipeee servers

---
+++ bot/test/talium/inputs/TipeeeStream/TipeeeConnectionTest.java
@@ -0,0 +1,38 @@
+package talium.inputs.TipeeeStream;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import talium.system.inputSystem.HealthManager;
+import talium.system.inputSystem.InputStatus;
+
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+@TestPropertySource("classpath:/application-test.properties")
+public class TipeeeConnectionTest {
+    @Autowired
+    TipeeeConfig config;
+
+    @Test
+    void connect() {
+        var donationRepo = Mockito.mock(DonationRepo.class);
+        try {
+            var tipeee = new TipeeeInput(config, donationRepo);
+            var shouldHaveFinishedBy = Instant.now().plusSeconds(10);
+            tipeee.startup();
+            while (HealthManager.get(TipeeeInput.class) == InputStatus.STARTING && shouldHaveFinishedBy.isAfter(Instant.now())) {
+                Thread.onSpinWait();
+            }
+            assertEquals(InputStatus.HEALTHY, HealthManager.get(TipeeeInput.class));
+            tipeee.shutdown();
+        } catch (TipeeeInput.TipeeeStreamException e) {
+            fail("Connection failed with: ", e);
+        }
+    }
+}
